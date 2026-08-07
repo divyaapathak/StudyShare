1ccea590ac02ca8309ff82b64cdb3b3c3023aaea
divyaapathak <divyapathakji06@gmail.com>
2026-08-07 22:07:27 +0530

    Added complete authentication with JWT

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
+// ================= REGISTER =================
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -36,6 +38,62 @@ export const registerUser = async (req, res) => {
     res.status(201).json({
       success: true,
       message: "Registration Successful",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// ================= LOGIN =================
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check empty fields
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all fields",
+      });
+    }
+
+    // Find user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid password",
+      });
+    }
+
+    // Generate JWT Token
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Login Successful",
+      token,
       user,
     });
   } catch (error) {
